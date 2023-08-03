5b1b16fba47bd21595f969fbcb2b146e9799bdeb
Walkah <walkerbabich@gmail.com>
2023-08-03 12:20:16 -0400

    adding src/components for templates

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,13 +1,31 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import AllPlants from './views/allPlants.vue';
+import Home from '@/components/Home.vue';
+import Search from '@/components/Search.vue';
+import ManagePlant from '@/components/ManagePlant.vue';
 
 const routes = [
   {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
     path: '/all-plants',
-    name: 'allPlants',
+    name: 'AllPlants',
     component: AllPlants
   },
-  // You can add more routes here as needed
+  {
+    path: '/search',
+    name: 'Search',
+    component: Search
+  },
+  {
+    path: '/manage-plant',
+    name: 'ManagePlant',
+    component: ManagePlant
+  }
+  // add more routes as needed
 ];
 
 const router = createRouter({
---
+++ src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="home">
+      <h1>Welcome to Our Plant App!</h1>
+      <p>Your go-to place for all things plants.</p>
+      <!-- Add more elements or details for your splash screen here -->
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Home'
+  }
+  </script>
+  
+  <style scoped>
+  .home {
+    text-align: center;
+    margin-top: 60px;
+  }
+  </style>
+  
